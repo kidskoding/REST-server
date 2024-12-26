8587d8c1cb4e80cfe99ad536d9efd6b0bd04470c
Anirudh Konidala <kidskodingclub@gmail.com>
2024-12-26 15:50:29 -0600

    Added integration with Mongo DB

---
+++ db.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var client *mongo.Client
+
+func connectDB() {
+	var err error
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err = mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+}
--- main.go
+++ main.go
@@ -2,20 +2,25 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func main() {
-	handleRequests()
-}
-
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Home Page!")
+	fmt.Fprint(w, "A REST server that tracks customers' pizza orders")
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
 	port := 8080
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+
 	fmt.Println("Server is running on port", port)
-	http.ListenAndServe(fmt.Sprintf("localhost:%d", port), nil)
+	http.ListenAndServe(fmt.Sprintf("localhost:%d", port), myRouter)
+}
+
+func main() {
+	connectDB()
+	handleRequests()
 }
